3537254af45281a79781d6e82f7fb046f20c48a6
Artem Novichkov <mail@artemnovichkov.com>
2023-03-27 15:21:21 +0600

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Artem Novichkov on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
